412da8bb0906e02af427f207da1fdfadf7852e4b
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2024-03-21 20:03:57 -0300

    criaçao do componente de group card

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,18 @@
-import { Highlight } from '@components/Highlight';
-import { Container } from './styles';
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+
+import { Container } from './styles';
 
 export const Groups = () => {
   return (
     <Container>
       <Header />
-      <Highlight 
+      <Highlight
         title='Turmas'
         subtitle='Jogue com a sua turma'
       />
+      <GroupCard title='Galera do Ignite'/>
     </Container>
   );
 };
