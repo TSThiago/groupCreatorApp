f40cd8f288e76232d35f645e9175082ea2da93fa
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2024-03-20 23:03:09 -0300

    criaçao do componente de loading

--- App.tsx
+++ App.tsx
@@ -1,16 +1,15 @@
-import { ActivityIndicator } from "react-native";
 import { Groups } from "@screens/Groups";
 import theme from "src/theme/theme";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { ThemeProvider } from "styled-components/native";
-
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 };
