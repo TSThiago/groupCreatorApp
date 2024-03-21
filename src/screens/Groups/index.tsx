e7a92899b0dac80e1d44b14df5ded5d57a1ff5ba
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2024-03-20 21:23:49 -0300

    inicio do projeto

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View } from 'react-native';
+import { Container, Title } from './styles';
+
+export const Groups = () => {
+  return (
+    <Container>
+      <Title>
+        Groups
+      </Title>
+    </Container>
+  );
+};
+
