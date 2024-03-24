1e1ea2c465fa91f4a80d3a7c42eae021879f9c51
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2024-03-23 22:28:44 -0300

    criaçao da screen de players

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import { Loading } from "@components/Loading";
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { ThemeProvider } from "styled-components/native";
+import { Players } from "@screens/Players";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 };
