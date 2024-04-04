8a56f7eb1e7af1738dfac94e616c3e41965622f0
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2024-04-03 22:42:01 -0300

    implementando o local storage de groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -9,15 +9,30 @@ import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
 import { Container } from './styles';
+import { groupsGetAll } from 'src/storage/group/groupsGetAll';
 
 export const Groups = () => {
   const [groups, setGroups] = useState<string[]>([]);
 
   const navigation = useNavigation();
 
+  const fetchGroups = async () => {
+    try {
+      const data = await groupsGetAll()
+      setGroups(data);
+    } catch (error) {
+      console.log(error);
+    };
+  };
+
   const handleNewGroup = () => {
     navigation.navigate('new')
-  }
+  };
+
+
+  useFocusEffect(useCallback(() => {
+    fetchGroups();
+  }, []));
 
   return (
     <Container>
